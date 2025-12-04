d30266c1918f6ce27cfa51e54a3fe91025a2b4cc
Anght <47832371+cauedrummond@users.noreply.github.com>
2025-12-04 22:00:15 +0000

    adicionando segunda frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>BOM DIA FAQSIONERS ESPERO QUE TODOS SOFRAM QUEIMADOS NAS PROFUNDEZAS DO &gt;complete aqui&lt;</h1>
+    return (
+        <div>
+            <h1>BOM DIA FAQSIONERS ESPERO QUE TODOS SOFRAM QUEIMADOS NAS PROFUNDEZAS DO &gt;complete aqui&lt;</h1>
+            <h3>acompanhem essa porra atualizaçoes diarias todos os dias (menos alguns)</h3>
+        </div>
+    )
 }
 
 export default Home;
