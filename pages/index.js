530aa4de5bf4735a5e4def6eefedebae0b0a41a9
Anght <47832371+cauedrummond@users.noreply.github.com>
2025-12-04 21:31:11 +0000

    correção na pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>BOM DIA FAQSIONERS ESPERO QUE TODOS SOFRAM QUEIMADOS NAS PROFUNDEZAS DO >complete aqui<</h1>
+    return <h1>BOM DIA FAQSIONERS ESPERO QUE TODOS SOFRAM QUEIMADOS NAS PROFUNDEZAS DO &gt;complete aqui&lt;</h1>
 }
 
 export default Home;
